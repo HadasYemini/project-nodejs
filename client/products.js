d961412af77bd7df12cd1dc3586997cf935f39eb
HadasYemini <hadassyemini@gmail.com>
2024-06-09 00:07:08 +0300

    sort, search and select - Fixed

--- client/products.js
+++ client/products.js
@@ -38,34 +38,41 @@ const getProducts = async () => {
 function initProducts(list) {
   const e = document.querySelector("table");
   if (e) e.remove();
-  //  products = list;
   const table = document.createElement("table");
   table.classList.add("table");
   const thead = document.createElement("thead");
   const th = document.createElement("th");
   for (let key in list[0]) {
-    const td = document.createElement("td");
-    td.textContent = key;
-    td.classList.add(key);
-    th.appendChild(td);
+    if (key !== "select") {
+      const td = document.createElement("td");
+      td.textContent = key;
+      td.classList.add(key);
+      th.appendChild(td);
+    }
   }
   thead.appendChild(th);
   const tbody = document.createElement("tbody");
   list.forEach((item, index) => {
     let product = tbody.insertRow();
-    product.id = item.name;
+    product.name = item.name;
     product.classList.add("product");
+    if (item.select) product.classList.add("select");
     product.onclick = () => {
+      console.log(products.find(p => p.name === product.name))
       if (product.classList.contains("select")) {
+        products.find(p => p.name === product.name).select = false
         product.classList.remove("select");
       } else {
+        products.find(p => p.name === product.name).select = true
         product.classList.add("select");
       }
     };
     for (let key in item) {
-      const elem = product.insertCell();
-      elem.textContent = item[key];
-      elem.classList.add(key);
+      if (key !== "select") {
+        const elem = product.insertCell();
+        elem.textContent = item[key];
+        elem.classList.add(key);
+      }
     }
   });
   let con = document.getElementById("products-con");
@@ -84,25 +91,12 @@ const getSelectedOption = () => {
 };
 
 const sortProducts = (id) => {
-  const sortedProducts = products.toSorted((a, b) => {
-    let elemA;
-    let elemB;
-    if (id === "price") {
-      elemA = Number(a[id]);
-      elemB = Number(b[id]);
-    } else {
-      elemA = a[id].toLowerCase();
-      elemB = b[id].toLowerCase();
-    }
-
-    if (elemA < elemB) {
-      return -1;
-    }
-    if (elemA > elemB) {
-      return 1;
-    }
-    return 0;
-  });
+  let sortedProducts 
+  if (id === 'name') {
+   sortedProducts = products.toSorted((a, b) => a.name.trim().toLowerCase().localeCompare(b.name.trim().toLowerCase()));
+  }else{
+    sortedProducts = products.toSorted((a, b) => parseFloat(a.price) - parseFloat(b.price));
+  }
   initProducts(sortedProducts);
 };
 
--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.use(bp.urlencoded());
 
 const db = require("mongoose");
 db.connect(
-  //"mongodb+srv://hadas:hy1234hy@cluster0.nefe6tn.mongodb.net/svshopDb"
-  "mongodb+srv://ynon:ChyEqc7VUc7GbxfV@cluster0.kdysbnh.mongodb.net/svshopDb" //ynon
+  "mongodb+srv://hadas:hy1234hy@cluster0.nefe6tn.mongodb.net/svshopDb"
+//  "mongodb+srv://ynon:ChyEqc7VUc7GbxfV@cluster0.kdysbnh.mongodb.net/svshopDb" //ynon
 );
 
 const userSchema = db.Schema({
@@ -23,6 +23,7 @@ const usersModel = db.model("users", userSchema);
 const productSchema = db.Schema({
   name: String,
   price: String,
+  select:Boolean
 });
 
 const productsModel = db.model("products", productSchema);
@@ -84,87 +85,153 @@ const productsArr = [
   {
     name: "Set of 4 rings",
     price: "132",
+    select: false
   },
   {
     name: "Ring",
     price: "76",
+    select: false
   },
   {
     name: "Set of 3 bracelets",
     price: "215",
+    select: false
   },
   {
     name: "Bracelet",
     price: "113",
+    select: false
   },
   {
     name: "Set of 3 necklace",
     price: "300",
+    select: false
   },
   {
     name: "Necklace",
     price: "125",
+    select: false
   },
   {
     name: "Set of 4 earrings",
     price: "200",
+    select: false
   },
   {
     name: "Earrings",
     price: "90",
+    select: false
   },
   {
     name: "Gold chain",
     price: "200",
+    select: false
   },
   {
     name: "Silver chain",
     price: "250",
+    select: false
   },
   {
     name: "Leg bracelet",
     price: "75",
+    select: false
   },
   {
     name: "Set of 5 earrings",
     price: "220",
+    select: false
   },
   {
     name: "Bag",
     price: "200",
+    select: false
   },
   {
     name: "Choker necklace",
     price: "199",
+    select: false
   },
   {
     name: "Glasses",
     price: "175",
+    select: false
   },
   {
     name: "Sunglasses",
     price: "97",
+    select: false
   },
   {
     name: " Bead necklace",
     price: "163",
+    select: false
   },
   {
     name: "Scarf",
     price: "68",
+    select: false
   },
   {
     name: "Hair bands",
     price: "10",
+    select: false
   },
   {
     name: "Leather Wallet",
     price: "156",
+    select: false
   },
   {
     name: "Wallet",
     price: "123",
+    select: false
   },
+  {
+    name: "Watch",
+    price: "123",
+    select: false
+  },
+  {
+    name: "Hat",
+    price: "123",
+    select: false
+  },
+  {
+    name: "Gloves",
+    price: "123",
+    select: false
+  },
+  {
+    name: "Socks",
+    price: "123",
+    select: false
+  },
+  {
+    name: "Umbrella",
+    price: "123",
+    select: false
+  },
+  {
+    name: "Shoes",
+    price: "123",
+    select: false
+  },
+  {
+    name: "Tie",
+    price: "123",
+    select: false
+  },
+  {
+    name: "Hair clip",
+    price: "123",
+    select: false
+  },
+  {
+    name: "Airpods",
+    price: "123",
+    select: false
+  }
 ];
 
 app.get("/products", async (req, res) => {
