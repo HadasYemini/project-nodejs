fc74d3451ea3c5f3f187067715d41564b07d2a8a
HadasYemini <hadassyemini@gmail.com>
2024-06-14 13:50:46 +0300

    removed const { truncate } = require("fs-extra");

--- server.js
+++ server.js
@@ -7,10 +7,10 @@ app.use(bp.json());
 app.use(bp.urlencoded());
 
 const db = require("mongoose");
-const { truncate } = require("fs-extra");
+
 db.connect(
- // "mongodb+srv://hadas:hy1234hy@cluster0.nefe6tn.mongodb.net/svshopDb"
-      "mongodb+srv://ynon:ChyEqc7VUc7GbxfV@cluster0.kdysbnh.mongodb.net/svshopDb" //ynon
+  "mongodb+srv://hadas:hy1234hy@cluster0.nefe6tn.mongodb.net/svshopDb"
+  //    "mongodb+srv://ynon:ChyEqc7VUc7GbxfV@cluster0.kdysbnh.mongodb.net/svshopDb" //ynon
 );
 
 const userSchema = db.Schema({
